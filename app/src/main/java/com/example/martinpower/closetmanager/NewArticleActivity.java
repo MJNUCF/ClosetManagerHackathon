92d65f7ea374ab644039257cb0e7d6d520af71db
MattNipper <matt.nipper97@gmail.com>
2017-11-18 03:08:21 -0500

    added everything

---
+++ app/src/main/java/com/example/martinpower/closetmanager/NewArticleActivity.java
@@ -0,0 +1,15 @@
+package com.example.martinpower.closetmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_article);
+    }
+
+
+}
